f934a40d10634d96e55c877fa54794321d3bfd3b
Adelina Jiang <adelinajiang24@gmail.com>
2026-04-29 22:47:59 -0400

    fix: declare ToString() virtual in DifferentialEquation base class

--- include/Oasis/DifferentialEquation.hpp
+++ include/Oasis/DifferentialEquation.hpp
@@ -2,61 +2,68 @@
 // Created by Adelina Jiang on 2/3/2026.
 //
 
+#ifndef OASIS_DIFFERENTIALEQUATION_HPP
+#define OASIS_DIFFERENTIALEQUATION_HPP
+
+#include "Oasis/Expression.hpp"
+#include "Oasis/Variable.hpp"
+#include <expected>
+#include <memory>
+#include <string>
+
 namespace Oasis {
 
-    enum class DECategory {
-        Unclassified,
-        FirstOrderHomogeneous,
-        FirstOrderLinear,
-        FirstOrderSeparable
-    };
+enum class DECategory {
+    Unclassified,
+    FirstOrderHomogeneous,
+    FirstOrderLinear,
+    FirstOrderSeparable
+};
 
-    class DifferentialEquation : public Expression {
-    public:
-        DifferentialEquation(
-            std::unique_ptr<Expression> lhs,
-            std::unique_ptr<Expression> rhs,
-            std::string independentVar = "x",
-            std::string dependentVar   = "y"
-        );
+class DifferentialEquation : public Expression {
+public:
+    DifferentialEquation(
+        std::unique_ptr<Expression> lhs,
+        std::unique_ptr<Expression> rhs,
+        std::string independentVar = "x",
+        std::string dependentVar   = "y");
 
-        DifferentialEquation(const DifferentialEquation&);
-        DifferentialEquation& operator=(const DifferentialEquation&);
-        DifferentialEquation(DifferentialEquation&&) noexcept            = default;
-        DifferentialEquation& operator=(DifferentialEquation&&) noexcept = default;
-        ~DifferentialEquation() override                                  = default;
+    DifferentialEquation(const DifferentialEquation&);
+    DifferentialEquation& operator=(const DifferentialEquation&);
+    DifferentialEquation(DifferentialEquation&&) noexcept            = default;
+    DifferentialEquation& operator=(DifferentialEquation&&) noexcept = default;
+    ~DifferentialEquation() override                                  = default;
 
-        [[nodiscard]] virtual std::expected<std::unique_ptr<Expression>, std::string>
-        Solve() const = 0;
+    [[nodiscard]] virtual std::expected<std::unique_ptr<Expression>, std::string>
+    Solve() const = 0;
 
-        [[nodiscard]] virtual DECategory Classify() const = 0;
+    [[nodiscard]] virtual DECategory Classify() const = 0;
 
-        [[nodiscard]] std::unique_ptr<Expression> Copy() const override;
-        [[nodiscard]] bool Equals(const Expression& other) const override;
-        [[nodiscard]] std::string ToString() const;
+    [[nodiscard]] std::unique_ptr<Expression> Copy() const override;
+    [[nodiscard]] bool Equals(const Expression& other) const override;
+    [[nodiscard]] virtual std::string ToString() const;
+    [[nodiscard]] virtual bool Verify(const Expression& solution) const;
 
-        [[nodiscard]] virtual bool Verify(const Expression& solution) const;
+    [[nodiscard]] static DECategory ClassifyDE(
+        const Expression& lhs,
+        const Expression& rhs,
+        const std::string& independentVar = "x",
+        const std::string& dependentVar   = "y");
 
-        [[nodiscard]] static DECategory ClassifyDE(
-            const Expression& lhs,
-            const Expression& rhs,
-            const std::string& independentVar = "x",
-            const std::string& dependentVar   = "y");
+    [[nodiscard]] const Expression&  GetLHS()            const { return *lhs_; }
+    [[nodiscard]] const Expression&  GetRHS()            const { return *rhs_; }
+    [[nodiscard]] const std::string& GetIndependentVar() const { return independentVar_; }
+    [[nodiscard]] const std::string& GetDependentVar()   const { return dependentVar_; }
+    [[nodiscard]] DECategory         GetDECategory()     const { return category_; }
 
-        [[nodiscard]] const Expression&  GetLHS()            const { return *lhs_; }
-        [[nodiscard]] const Expression&  GetRHS()            const { return *rhs_; }
-        [[nodiscard]] const std::string& GetIndependentVar() const { return independentVar_; }
-        [[nodiscard]] const std::string& GetDependentVar()   const { return dependentVar_; }
-        [[nodiscard]] DECategory         GetDECategory()     const { return category_; }
-
-    protected:
-        std::unique_ptr<Expression> lhs_;
-        std::unique_ptr<Expression> rhs_;
-        std::string                 independentVar_;
-        std::string                 dependentVar_;
-        DECategory                  category_ { DECategory::Unclassified };
-    };
+protected:
+    std::unique_ptr<Expression> lhs_;
+    std::unique_ptr<Expression> rhs_;
+    std::string                 independentVar_;
+    std::string                 dependentVar_;
+    DECategory                  category_ { DECategory::Unclassified };
+};
 
 } // namespace Oasis
 
-#endif //OASIS_DIFFERENTIALEQUATION_HPP
+#endif // OASIS_DIFFERENTIALEQUATION_HPP
